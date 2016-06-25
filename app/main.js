46437f5ae0034f3d37c453f89fe84d28a698ccdb
Mark <mark.lishman@gmail.com>
2016-06-25 11:26:27 +0100

    test

---
+++ app/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var hello_world_component_1 = require('./hello-world.component');
+platform_browser_dynamic_1.bootstrap(hello_world_component_1.HelloWorldComponent);
+//# sourceMappingURL=main.js.map
