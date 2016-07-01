1674b7ef99d2a06ea9de7ee3bfc289801d3877c1
Mark <mark.lishman@gmail.com>
2016-07-01 07:03:14 +0100

    Restored .ts files (deleted by mistake). Removed .map and .js files

--- app/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var hello_world_component_1 = require('./hello-world.component');
-platform_browser_dynamic_1.bootstrap(hello_world_component_1.HelloWorldComponent);
-//# sourceMappingURL=main.js.map
