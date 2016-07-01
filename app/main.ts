145b8954dc768afe522c63d8bd17fef58e3bc8a4
Mark <mark.lishman@gmail.com>
2016-07-01 07:07:26 +0100

    Code formatting

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import {HelloWorldComponent} from './hello-world.component';
+
+import { HelloWorldComponent } from './hello-world.component';
 
 bootstrap(HelloWorldComponent);
