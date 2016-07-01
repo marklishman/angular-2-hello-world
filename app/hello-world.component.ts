4362e5b56be84a7fc24fa2df2ac339f4c7573da2
Mark <mark.lishman@gmail.com>
2016-07-01 06:56:59 +0100

    Removed .map and .js files

--- app/hello-world.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'hello-world',
-    template: '<h1>Hello {{name}}!</h1>'
-})
-export class HelloWorldComponent {
-    private name = "World";
-}
--- app/main.ts
+++
@@ -1,4 +0,0 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import {HelloWorldComponent} from './hello-world.component';
-
-bootstrap(HelloWorldComponent);
